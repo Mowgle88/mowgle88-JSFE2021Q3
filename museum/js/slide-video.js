e368b638f9c0ca7d57f7e6e5b40dd209715cb194
Mowgle88 <mowgle1988@gmail.com>
2021-10-11 00:30:23 +0300

    feat: add gallery-slide

--- museum/js/slide-video.js
+++ museum/js/slide-video.js
@@ -1,43 +1,91 @@
+if (document.documentElement.clientWidth < 950) {
 
-var swiper = new Swiper(".mySwiper", {
-  loop: true,
-  freeMode: false,
-  // spaceBetween: 10,
-  navigation: {
-    nextEl: ".video-slider-right",
-    prevEl: ".video-slider-right",
-  },
-  thumbs: {
-    swiper: swiper2,
-  },
-});
-
-var swiper2 = new Swiper(".mySwiper2", {
-  slidesPerView: 3,
-  spaceBetween: 41,
-  loop: true,
-
-  // loopedSlides: 3,
-  // loopAdditionalSlides: 1,
-  // loopPreventsSlide: true,
-
+  var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    freeMode: false,
+    navigation: {
+      nextEl: ".video-slider-right",
+      prevEl: ".video-slider-left",
+    },
+    thumbs: {
+      swiper: swiper2,
+    },
+  });
   
-  freeMode: false,
-  watchSlidesProgress: true,
+  var swiper2 = new Swiper(".mySwiper2", {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    loop: true,
+    // loopedSlides: 3,
+    // loopAdditionalSlides: 1,
+    // loopPreventsSlide: true,
+    freeMode: false,
+    watchSlidesProgress: true,
+    slidesPerGroup: 1,
+    // loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".video-slider-dots-items",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".video-slider-right",
+      prevEl: ".video-slider-left",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+  
+  });
 
-  slidesPerGroup: 1,
-  // loopFillGroupWithBlank: true,
+} else {
 
-  pagination: {
-    el: ".video-slider-dots-items",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".video-slider-right",
-    prevEl: ".video-slider-left",
-  },
-  thumbs: {
-    swiper: swiper,
-  },
+  var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    freeMode: false,
+    navigation: {
+      nextEl: ".video-slider-right",
+      prevEl: ".video-slider-left",
+    },
+    thumbs: {
+      swiper: swiper2,
+    },
+  });
+  
+  var swiper2 = new Swiper(".mySwiper2", {
+    slidesPerView: 3,
+    spaceBetween: 42,
+    loop: true,
+    // loopedSlides: 3,
+    // loopAdditionalSlides: 1,
+    // loopPreventsSlide: true,
+    freeMode: false,
+    watchSlidesProgress: true,
+    slidesPerGroup: 1,
+    // loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".video-slider-dots-items",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".video-slider-right",
+      prevEl: ".video-slider-left",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+  
+  });
 
-});
+}
+
+
+// window.addEventListener("resize", function() {
+//   if( window.innerWidth <= 950 ){
+//     console.log("Resource conscious resize callback!");
+//     swiper.slidesPerView = 2;
+//   }
+// });
+
+
+
+
