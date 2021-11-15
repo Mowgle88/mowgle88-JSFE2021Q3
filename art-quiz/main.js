7bc8366317d1a80a729ed264dad1a11b93e83e41
Mowgle88 <mowgle1988@gmail.com>
2021-11-16 01:25:10 +0300

    feat: added title-page

---
+++ art-quiz/main.js
@@ -0,0 +1,30 @@
+// async function question() {    
+//   const images = './images.json';
+//   const res = await fetch(images);
+//   const data = await res.json(); 
+//   const randomNumImages = Math.floor(Math.random() * data.length);
+//   let imageNum = data[randomNumImages]["imageNum"]
+
+//   console.log(data[randomNumImages]["author"]);
+//   console.log(data[randomNumImages]["name"]);
+//   console.log(data[randomNumImages]["year"]);
+//   console.log(data[randomNumImages]["imageNum"]);
+
+//   const picture = document.querySelector('.div');
+//   function addPuzzle() {
+//     const img = document.createElement('img');
+//     const h = document.createElement('h1');
+//     const p = document.createElement('h2');
+//     h.textContent = data[randomNumImages]["author"];
+//     p.textContent = `${data[randomNumImages]["name"]}, ${data[randomNumImages]["year"]}`;
+//     img.src = `img/${imageNum}.webp`;
+//     img.alt = `${imageNum}`;
+//     picture.append(img);
+//     picture.append(h);
+//     picture.append(p);
+//   }
+//   addPuzzle()
+// }
+
+// question()
+
