219d3773a523730b892ef07557c0d697fa29b10c
Mowgle88 <mowgle1988@gmail.com>
2021-12-07 03:15:35 +0300

    fix: replace url in appLoader.js

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
   
   constructor() {
-    super('https://newsapi.org/v2/', {
+    super('https://nodenews.herokuapp.com/', {  //https://nodenews.herokuapp.com/  and https://newsapi.org/v2/
       apiKey: 'a3d81b61bb74422ab15d213f6304c38d', // получите свой ключ https://newsapi.org/
     });
   }
