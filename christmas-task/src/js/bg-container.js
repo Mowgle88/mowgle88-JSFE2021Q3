b059a80def45a3145d0a8b4ac090269ac5ad301a
Mowgle88 <mowgle1988@gmail.com>
2021-12-26 21:29:42 +0300

    feat: add the function to change the background

--- christmas-task/src/js/bg-container.js
+++ christmas-task/src/js/bg-container.js
@@ -1,4 +1,5 @@
 const bgContainer = document.querySelector('.bg-container');
+const mainContainer = document.querySelector('.main-container');
 
 let i = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
@@ -6,12 +7,34 @@ function addBg(ind) {
   const div = document.createElement('div');
   div.classList.add('cell');
   div.classList.add('bg');
+  if (ind === 1) {
+    div.classList.add('bg-active');
+  }
   div.setAttribute(`data-bg`, `${ind}`);
   div.style.backgroundImage = `url('./assets/bg/${ind}.webp')`;
 
   bgContainer.append(div);
 }
 
-for (let j = 0; j < i.length; j++) {
-  addBg(i[j]);
+i.forEach((n) => addBg(n));
+
+// Менять фон
+
+const bg = document.querySelectorAll('.bg');
+
+function changeBg(event) {
+  let target = event.target;
+  let ind = target.getAttribute('data-bg');
+
+  if (target.classList.contains('bg')) {
+    bg.forEach((el) => {
+      el.classList.remove('bg-active');
+    });
+    target.classList.add('bg-active');
+    mainContainer.style.backgroundImage = `url('./assets/bg/${ind}.webp')`;
+  }
 }
+
+bgContainer.addEventListener('click', (e) => {
+  changeBg(e);
+});
