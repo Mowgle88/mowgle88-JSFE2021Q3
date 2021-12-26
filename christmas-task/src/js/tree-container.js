6a2add94e64ad38382ffc997d09498d7bc8be1ac
Mowgle88 <mowgle1988@gmail.com>
2021-12-26 21:21:34 +0300

    feat: add the function to change the tree

--- christmas-task/src/js/tree-container.js
+++ christmas-task/src/js/tree-container.js
@@ -1,4 +1,5 @@
 const treeContainer = document.querySelector('.tree-container');
+const mainTree = document.querySelector('.main-tree');
 
 let i = [1, 2, 3, 4, 5, 6];
 
@@ -6,12 +7,35 @@ function addTree(ind) {
   const div = document.createElement('div');
   div.classList.add('cell');
   div.classList.add('tree');
+  if (ind === 1) {
+    div.classList.add('tree-active');
+  }
   div.setAttribute(`data-tree`, `${ind}`);
   div.style.backgroundImage = `url('./assets/tree/${ind}.webp')`;
 
   treeContainer.append(div);
 }
 
-for (let j = 0; j < i.length; j++) {
-  addTree(i[j]);
+i.forEach((n) => addTree(n));
+
+// Менять елочку
+
+const tree = document.querySelectorAll('.tree');
+
+function changeTree(event) {
+  let target = event.target;
+  let ind = target.getAttribute('data-tree');
+
+  if (target.classList.contains('tree')) {
+    tree.forEach((el) => {
+      el.classList.remove('tree-active');
+    });
+    target.classList.add('tree-active');
+    mainTree.src = `./assets/tree/${ind}.webp`;
+    mainTree.alt = `${ind}`;
+  }
 }
+
+treeContainer.addEventListener('click', (e) => {
+  changeTree(e);
+});
