ccec3d91efe24d6026c5e602d7231060a9c614fd
Mowgle88 <mowgle1988@gmail.com>
2022-01-30 21:24:53 +0300

    feat: add url heroku

--- async-race/src/components/methods/call-api.ts
+++ async-race/src/components/methods/call-api.ts
@@ -2,7 +2,8 @@ import axios, { Method } from 'axios';
 import { Car } from './interfaces';
 
 export const callApi = async (method: Method, url: string, body?: Car) => {
-  const baseUrl: string = 'http://localhost:3000';
+  // const baseUrl: string = 'http://localhost:3000';
+  const baseUrl: string = 'https://rocky-badlands-93860.herokuapp.com';
   const res = await axios({
     method,
     url: `${baseUrl}${url}`,
